e3049743ed8e8a0dd77916b48976598da0c509a3
angeldove <dev.roohiverma@gmail.com>
2017-03-09 23:29:17 +0530

    config params etc

---
+++ src/com/weavebytes/config/Config.java
@@ -0,0 +1,40 @@
+/*
+ * -----------------------------------
+ * Lan Chat Lite
+ * -----------------------------------
+ *
+ * This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * 
+ * Author: Roohi Verma <roohivermapb@gmail.com>
+ * 
+ */
+
+
+package com.weavebytes.config;
+
+/**
+ * Class to store various configuration settings for project
+ * 
+ * @author weavebytes
+ *
+ */
+public class Config {
+
+	public static int UDP_PORT = 5005;
+	
+	public static int TCP_PORT = 5000;
+	
+	
+}//Config
